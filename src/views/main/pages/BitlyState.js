d7d946c9321792a23071ac41e8e48879168fc324
Teakongnoo <tea.kongnoo@gmail.com>
2023-04-11 21:03:40 +0700

    test

--- src/views/main/pages/BitlyState.js
+++ src/views/main/pages/BitlyState.js
@@ -21,59 +21,11 @@ import Paper from 'src/@core/theme/overrides/paper'
 const FormLayoutsIcons = () => {
   // Hook useState
   const [datas, setDatas] = useState([])
-  const [botId, setBotId] = useState('')
-  const [ownerId, setOwnerId] = useState('')
-  const [accessToken, setAccessToken] = useState('')
-  const [secretKey, setSecretKey] = useState('')
-  const [formValid, setFormValid] = useState(false)
-
-  const inputBotId = event => {
-    setBotId(event.target.value)
-  }
-
-  const inputOwnerId = event => {
-    setOwnerId(event.target.value)
-  }
-
-  const inputAccessToken = event => {
-    setAccessToken(event.target.value)
-  }
-
-  const inputSecretKey = event => {
-    setSecretKey(event.target.value)
-  }
-
-  const onChange = () => {
-    console.log('1234')
-  }
-
-  const onClick = event => {
-    event.preventDefault()
-
-    const formData = {
-      botId: botId,
-      ownerId: ownerId,
-      accessToken: accessToken,
-      secretKey: secretKey
-    }
-    console.log(formData)
-    setBotId('')
-    setOwnerId('')
-    setAccessToken('')
-    setSecretKey('')
-  }
-
-  // useEffect(() => {
-  //   const formCheck  = botId.trim().length > 0 ?setFormValid(true) : setFormValid(false);
-  // }, [botId])
-
   useEffect(() => {
-    // Update the document title using the browser API
-    const formCheck = botId.trim().length > 0 ? setFormValid(true) : setFormValid(false)
-
     fetch('https://www.melivecode.com/api/users')
       .then(res => res.json())
       .then(result => {
+        console.log(result)
         setDatas(result)
       })
   })
@@ -81,8 +33,8 @@ const FormLayoutsIcons = () => {
   return (
     <Grid container spacing={2}>
       {datas.map(data => (
-        <Grid item xs={6} md={6} key={data.id}>
-          <Card md={6} variant='outlined' key={data.id}>
+        <Grid item xs={6} key={data.id}>
+          <Card variant='outlined' key={data.id}>
             <CardHeader
               avatar={<Avatar alt={data.fname} src={data.avatar} />}
               title={data.fname}
@@ -90,8 +42,7 @@ const FormLayoutsIcons = () => {
             />
             <CardContent>
               <Typography variant='body2' color='text.secondary'>
-                This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1
-                cup of frozen peas along with the mussels, if you like.
+                {data.avatar}
               </Typography>
             </CardContent>
             <CardContent>
